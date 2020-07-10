e8d62b94b873aaceebde1b0fc5a5075c31404f08
mkumar <36654337+ManojBishnoi@users.noreply.github.com>
2020-07-10 15:17:09 -0700

    Removed Multiple choice quetions

--- DANA_4830_Project.R
+++ DANA_4830_Project.R
@@ -161,4 +161,26 @@ vis_miss(mastercopy2)
 #Finally import the Final data set
 write.csv(mastercopy2,"final.csv", row.names = FALSE)
 
+testSet <- read.csv("final.csv")
+#removing Q5
+New.testSet <- testSet[,!grepl("Q5", colnames(testSet))]
+
+# Q12
+New.testSet <- New.testSet[,!grepl("Q12", colnames(New.testSet))]
+
+#Q13
+New.testSet <- New.testSet[,!grepl("Q13", colnames(New.testSet))]
+
+#Q15
+New.testSet <- New.testSet[,!grepl("Q15", colnames(New.testSet))]
+
+#Q16
+New.testSet <- New.testSet[,!grepl("Q16", colnames(New.testSet))]
+
+#Q19
+New.testSet <- New.testSet[,!grepl("Q19", colnames(New.testSet))]
+
+New.testSet
+summary(testSet["Q10C6"])
+
 
